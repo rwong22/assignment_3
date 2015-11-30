470c5c89b0f051e497b75cb84b85d172095b0ffe
rwong22 <silencerz_x@hotmail.com>
2015-11-29 20:11:56 -0800

    something

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,9 +2,9 @@ Rails.application.configure do
 
   require 'pusher'
 
-  Pusher.app_id = '153881'
-    Pusher.key = 'f718bef729fd9d06149e'
-    Pusher.secret = '55d1eb74e4870e0f75f4'
+  Pusher.app_id = '152250'
+    Pusher.key = '17dcd88da12484af9044'
+    Pusher.secret = '84eb8afad6a1eecea19a'
 
   # Settings specified here will take precedence over those in config/application.rb.
 
